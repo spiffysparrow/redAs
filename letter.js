01d4fe1b8c0668f895fd3d883e16026b842fb984
Tracy <tracy@Tracys-MacBook-Pro.local>
2015-11-29 14:39:53 -0800

    reorganized

---
+++ letter.js
@@ -0,0 +1,43 @@
+var lib = {}
+
+lib.myAnimation = function(canvas){
+
+  var c = canvas.getContext('2d');
+  var a1 = new lib.LetterA([100, 100], 4);
+  a1.render(c);
+
+  var a2 = new lib.LetterA([200, 200], 9);
+  a2.render(c);
+  a2.move(50, 50)
+  a2.render(c)
+
+}
+
+
+lib.LetterA = function(pos, size){
+  this.size = size;
+  this.pos = pos;
+}
+
+lib.LetterA.prototype.render = function (c) {
+  console.log(c)
+  c.fillStyle = "red";
+  var x = this.pos[0]
+  var y = this.pos[1]
+  var size = this.size
+  c.beginPath();
+  c.moveTo(x, y);
+  c.lineTo(x + 10 * size, y - 20 * size);
+  c.lineTo(x + 20 * size, y);
+  c.moveTo(x + 2.5 * size, y - 5 * size);
+  c.lineTo(x + 20 * size, y - 5 * size);
+  c.lineWidth = 4;
+  c.strokeStyle = 'red';
+  c.stroke();
+};
+
+
+lib.LetterA.prototype.move = function (dx, dy) {
+  this.pos[0] += dx
+  this.pos[1] += dy
+};
--- script.js
+++ script.js
@@ -1,43 +1,8 @@
 console.log("boo")
 
-function myAnimation(canvas){
+function run(canvas){
 
-  var c = canvas.getContext('2d');
-  var a1 = new LetterA([100, 100], 4);
-  a1.render(c);
+  console.log(lib)
+  lib.myAnimation(canvas);
 
-  var a2 = new LetterA([200, 200], 9);
-  a2.render(c);
-  a2.move(50, 50)
-  a2.render(c)
-
-}
-
-
-function LetterA(pos, size){
-  this.size = size;
-  this.pos = pos;
-}
-
-LetterA.prototype.render = function (c) {
-  console.log(c)
-  c.fillStyle = "red";
-  var x = this.pos[0]
-  var y = this.pos[1]
-  var size = this.size
-  c.beginPath();
-  c.moveTo(x, y);
-  c.lineTo(x + 10 * size, y - 20 * size);
-  c.lineTo(x + 20 * size, y);
-  c.moveTo(x + 2.5 * size, y - 5 * size);
-  c.lineTo(x + 20 * size, y - 5 * size);
-  c.lineWidth = 4;
-  c.strokeStyle = 'red';
-  c.stroke();
-};
-
-
-LetterA.prototype.move = function (dx, dy) {
-  this.pos[0] += dx
-  this.pos[1] += dy
 };
