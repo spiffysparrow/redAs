d5c5c02af15f5756fb8b61bcaaf23fbc7cfd8173
Tracy <tracy@Tracys-MacBook-Pro.local>
2015-11-29 21:15:01 -0800

    refactor

---
+++ game.js
@@ -0,0 +1,116 @@
+
+lib.totalAs = 0
+lib.pageConstants = []
+
+window.onload = function(){
+  console.log("loaded!")
+  document.onmousedown = lib.clickDraw;
+}
+
+lib.clickDraw = function(e) {
+  console.log("--Total As " + lib.totalAs);
+
+  var randCirclePos = lib.getRandomPointOnCircle()
+  var angle = randCirclePos[2]
+  randCirclePos = randCirclePos.slice(0, 2)
+
+  lib.renderAsOnCircle(randCirclePos, angle);
+
+  lib.lineToPoint([e.pageX, e.pageY], randCirclePos)
+
+  lib.totalAs++;
+}
+
+lib.renderAsOnCircle = function(randCirclePos, angle) {
+  var a1 = new LetterA(randCirclePos, 1);
+  a1.angle = angle;
+  a1.render()
+  lib.createRing(a1)
+}
+
+lib.lineToPoint = function(start, end){
+  var c = lib.canvas
+  c.beginPath();
+  c.moveTo(start[0], start[1])
+  c.lineTo(end[0], end[1]);
+  c.lineWidth = 1;
+  c.strokeStyle = 'red';
+  c.stroke();
+}
+
+lib.createRing = function(subject) {
+
+  function addOneA(angle, i, negative){
+    var newPos = lib.findCirclePosOffset(subject.angle, .05*i*i * negative)
+    var a1 = new LetterA(newPos, 1);
+    a1.render()
+    lib.pageConstants.push(a1)
+  }
+
+  function addEachA(angle, i){
+    addOneA(angle, i, 1)
+    addOneA(angle, i, -1)
+
+    i++;
+    if(i < 8){
+      setTimeout(function(){
+        addEachA(angle, i);
+      }, 50);
+    }
+  }
+
+  addEachA(subject.angle, 0);
+}
+
+
+lib.findCirclePosOffset = function(startAngle, offset) {
+  var angle = startAngle + offset
+  var pos = lib.getPointOnCircle(angle)
+  return pos
+}
+
+lib.getPointOnCircle = function(angle){
+  var center = [300, 300]
+  var radius = 200;
+  var x = Math.cos(angle)*radius;
+  var y = Math.sin(angle)*radius;
+  x = Math.round(x)
+  y = Math.round(y)
+  return [x + center[0], y + center[1]]
+}
+
+lib.getRandomPointOnCircle = function() {
+  var angle = Math.random()*Math.PI*2;
+  var pos = lib.getPointOnCircle(angle)
+  return [pos[0], pos[1], angle]
+}
+
+
+lib.myAnimation = function(canvas){
+  var c = canvas.getContext('2d');
+  lib.canvas = c
+  var a2 = new LetterA([200, 200], 3);
+  lib.fall(a2);
+}
+
+lib.fall = function(subject, moved) {
+  var moved = moved || 0
+  var c = lib.canvas;
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  subject.move(moved, moved)
+  moved++;
+  subject.render(c)
+  lib.drawPageConstants()
+  if(subject.pos[0] < 800){
+    requestAnimationFrame(function(){
+      lib.fall(subject, moved)
+    })
+  }
+}
+
+lib.drawPageConstants = function() {
+  lib.pageConstants.forEach(function(item){
+    var c = lib.canvas
+    item.render(c)
+  });
+};
--- letter.js
+++ letter.js
@@ -1,121 +1,7 @@
+
 var lib = {}
 
-lib.totalAs = 0
-lib.pageConstants = []
-
-lib.clickDraw = function(e) {
-  console.log("--Total As " + lib.totalAs);
-  var pos = lib.getRandomPointOnCircle()
-  var angle = pos[2]
-  pos = pos.slice(0, 2)
-  var a1 = new lib.LetterA(pos, 1);
-  a1.angle = angle;
-  a1.render()
-  lib.createRing(a1)
-  var c = lib.canvas
-  c.beginPath();
-  c.moveTo(e.pageX, e.pageY)
-  c.lineTo(pos[0], pos[1]);
-  c.lineWidth = 1;
-  c.strokeStyle = 'red';
-  c.stroke();
-
-  lib.totalAs++;
-
-}
-
-window.onload = function(){
-  console.log("loaded!")
-  document.onmousedown = lib.clickDraw;
-}
-
-lib.fall = function(subject, moved) {
-  var moved = moved || 0
-  var c = lib.canvas;
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  subject.move(moved, moved)
-  moved++;
-  subject.render(c)
-  lib.drawPageConstants()
-  if(subject.pos[0] < 800){
-    requestAnimationFrame(function(){
-      lib.fall(subject, moved)
-    })
-  }
-}
-
-lib.createRing = function(subject) {
-
-  function addA(angle, i){
-    var newPos = lib.findCirclePosOffset(subject.angle, .05*i*i)
-    var a1 = new lib.LetterA(newPos, 1);
-    a1.render()
-    lib.pageConstants.push(a1)
-
-    var newPos = lib.findCirclePosOffset(subject.angle, .05*i*i*-1)
-    var a2 = new lib.LetterA(newPos, 1);
-    a2.render()
-    lib.pageConstants.push(a2)
-    console.log(i)
-
-    i++;
-    if(i < 8){
-      setTimeout(function(){
-        addA(angle, i);
-      }, 50)
-    }
-  }
-
-  addA(subject.angle, 0);
-}
-
-
-
-lib.findCirclePosOffset = function(startAngle, offset) {
-  var center = [300, 300]
-  var radius = 200;
-  var angle = startAngle + offset
-  var x = Math.cos(angle)*radius;
-  var y = Math.sin(angle)*radius;
-  x = Math.round(x)
-  y = Math.round(y)
-  var newPos = [x + center[0], y + center[1]]
-  return newPos
-}
-
-
-lib.drawPageConstants = function() {
-  lib.pageConstants.forEach(function(item){
-    var c = lib.canvas
-    item.render(c)
-  });
-};
-
-
-lib.getRandomPointOnCircle = function() {
-  var center = [300, 300]
-  var radius = 200;
-  var angle = Math.random()*Math.PI*2;
-  var x = Math.cos(angle)*radius;
-  var y = Math.sin(angle)*radius;
-  x = Math.round(x)
-  y = Math.round(y)
-  return [x + center[0], y + center[1], angle]
-}
-
-
-lib.myAnimation = function(canvas){
-
-  var c = canvas.getContext('2d');
-  lib.canvas = c
-  var a2 = new lib.LetterA([200, 200], 3);
-  lib.fall(a2);
-
-}
-
-
-
-lib.LetterA = function(pos, size){
+LetterA = function(pos, size){
   this.size = size;
   var shadeOfRed = Math.floor(Math.random()*70)
   this.color = "rgba(158, "+ shadeOfRed +", 22, 1)"
@@ -126,7 +12,7 @@ lib.LetterA = function(pos, size){
   this.currentMove = null
 }
 
-lib.LetterA.prototype.render = function() {
+LetterA.prototype.render = function() {
   var c = lib.canvas
   c.fillStyle = this.color;
   var x = this.pos[0]
@@ -145,7 +31,7 @@ lib.LetterA.prototype.render = function() {
 };
 
 
-lib.LetterA.prototype.move = function (dx, dy) {
+LetterA.prototype.move = function (dx, dy) {
   this.pos[0] += dx
   this.pos[1] += dy
 };
