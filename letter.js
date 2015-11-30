d16874e5a2e707019b0699cf40595b5af4065ca8
Tracy <tracy@Tracys-MacBook-Pro.local>
2015-11-29 20:49:28 -0800

    click adds line and A appers there and cascades

--- letter.js
+++ letter.js
@@ -1,26 +1,33 @@
 var lib = {}
 
 lib.totalAs = 0
+lib.pageConstants = []
 
-lib.mousealert = function(e) {
+lib.clickDraw = function(e) {
   console.log("--Total As " + lib.totalAs);
   var pos = lib.getRandomPointOnCircle()
   var angle = pos[2]
   pos = pos.slice(0, 2)
-  var a1 = new lib.LetterA([e.pageX, e.pageY], 1);
+  var a1 = new lib.LetterA(pos, 1);
   a1.angle = angle;
+  a1.render()
+  lib.createRing(a1)
   var c = lib.canvas
   c.beginPath();
   c.moveTo(e.pageX, e.pageY)
   c.lineTo(pos[0], pos[1]);
-  c.lineWidth = 4;
+  c.lineWidth = 1;
   c.strokeStyle = 'red';
   c.stroke();
-  lib.fall_to(a1, pos)
+
   lib.totalAs++;
 
 }
-document.onmousedown = lib.mousealert;
+
+window.onload = function(){
+  console.log("loaded!")
+  document.onmousedown = lib.clickDraw;
+}
 
 lib.fall = function(subject, moved) {
   var moved = moved || 0
@@ -76,56 +83,6 @@ lib.findCirclePosOffset = function(startAngle, offset) {
   return newPos
 }
 
-lib.fall_to = function(subject, destination) {
-
-  if( subject.streek == 0 ){
-    subject.streek = Math.floor(Math.random() * 15) + 5
-
-    var move = [0, 0]
-    if (destination[0] > subject.pos[0]){
-      move[0] = 1
-    }else{
-      move[0] = -1
-    }
-
-    if (destination[1] > subject.pos[1]){
-      move[1] = 1
-    }else{
-      move[1] = -1
-    }
-
-    if( Math.random() > .5){
-      move[0] = 0
-    }else{
-      move[1] = 0
-    }
-
-    subject.currentMove = move
-  }
-
-  var currentMove = subject.currentMove;
-
-  var closeEnough = Math.abs(destination[0] - subject.pos[0]) + Math.abs(destination[1] - subject.pos[1])
-  if(closeEnough < 7){
-    currentMove = [0, 0]
-    subject.pos = destination
-    lib.pageConstants.push(subject)
-    lib.createRing(subject)
-    return
-  }
-
-  var c = lib.canvas;
-  // c.clearRect(0, 0, canvas.width, canvas.height);
-  subject.move(currentMove[0], currentMove[1])
-  subject.streek--;
-  subject.render(c)
-  lib.drawPageConstants()
-  requestAnimationFrame(function(){
-    lib.fall_to(subject, destination)
-  })
-}
-
-lib.pageConstants = []
 
 lib.drawPageConstants = function() {
   lib.pageConstants.forEach(function(item){
@@ -133,6 +90,7 @@ lib.drawPageConstants = function() {
     item.render(c)
   });
 };
+
 
 lib.getRandomPointOnCircle = function() {
   var center = [300, 300]
@@ -159,15 +117,18 @@ lib.myAnimation = function(canvas){
 
 lib.LetterA = function(pos, size){
   this.size = size;
+  var shadeOfRed = Math.floor(Math.random()*70)
+  this.color = "rgba(158, "+ shadeOfRed +", 22, 1)"
+  console.log("rgba(158, "+ shadeOfRed +", 22, 1)");
   this.pos = pos;
   this.angle = null
   this.streek = 0;
   this.currentMove = null
 }
 
-lib.LetterA.prototype.render = function (c) {
+lib.LetterA.prototype.render = function() {
   var c = lib.canvas
-  c.fillStyle = "red";
+  c.fillStyle = this.color;
   var x = this.pos[0]
   var y = this.pos[1]
   var size = this.size
@@ -178,8 +139,9 @@ lib.LetterA.prototype.render = function (c) {
   c.moveTo(x + 2.5 * size, y - 5 * size);
   c.lineTo(x + 20 * size, y - 5 * size);
   c.lineWidth = 4;
-  c.strokeStyle = 'red';
+  c.strokeStyle = this.color;
   c.stroke();
+  lib.totalAs++;
 };
 
 
